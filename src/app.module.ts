f9ecce5daeef2e1ed7376d398d3228755fbb78bc
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-03-05 23:07:05 +0000

    Fix import path for envSchema in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { envSchema } from "src/env";
 import { CreateAccountController } from "./controllers/create-account.controller";
+import { envSchema } from "./env";
 import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
