76e161637c4692cd9e1d03d2e5a413ec9feb7ec3
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-03-06 20:25:46 +0000

    Add authentication module with JWT support and environment validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "src/env";
+import { AuthModule } from "./auth/auth.module";
 import { CreateAccountController } from "./controllers/create-account.controller";
-import { envSchema } from "./env";
 import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
@@ -10,6 +11,7 @@ import { PrismaService } from "./prisma/prisma.service";
 			validate: (env) => envSchema.parse(env),
 			isGlobal: true,
 		}),
+		AuthModule,
 	],
 	controllers: [CreateAccountController],
 	providers: [PrismaService],
