02e34e927365bbf4e29267f55085c666b4fa3d84
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-03-06 20:58:46 +0000

    Add JWT key configuration and implement AuthenticateController for session management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { envSchema } from "src/env";
 import { AuthModule } from "./auth/auth.module";
+import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateAccountController } from "./controllers/create-account.controller";
 import { PrismaService } from "./prisma/prisma.service";
 
@@ -13,7 +14,7 @@ import { PrismaService } from "./prisma/prisma.service";
 		}),
 		AuthModule,
 	],
-	controllers: [CreateAccountController],
+	controllers: [CreateAccountController, AuthenticateController],
 	providers: [PrismaService],
 })
 export class AppModule {}
