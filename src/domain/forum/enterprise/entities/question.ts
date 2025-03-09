e85a058fc1fa975de5a865d86a5963e829c08209
Ivan Seibel <me@ivanseibel.dev>
2025-03-09 15:57:08 +0000

    Add answers table and user role enum, update question model for best answer

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,6 +1,6 @@
 import { AggregateRoot } from "@/core/entities/aggregate-root";
-import type { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import type { Optional } from "@/core/types/optional";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
 import { QuestionBestAnswerChosenEvent } from "@/domain/forum/enterprise/events/question-best-answer-chosen-event";
 import dayjs from "dayjs";
 import { QuestionAttachmentList } from "./question-attachment-list";
@@ -8,7 +8,7 @@ import { Slug } from "./value-objects/slug";
 
 export interface QuestionProps {
 	authorId: UniqueEntityID;
-	bestAnswerId?: UniqueEntityID;
+	bestAnswerId?: UniqueEntityID | null;
 	title: string;
 	content: string;
 	slug: Slug;
@@ -26,12 +26,44 @@ export class Question extends AggregateRoot<QuestionProps> {
 		return this.props.bestAnswerId;
 	}
 
+	set bestAnswerId(bestAnswerId: UniqueEntityID | undefined | null) {
+		if (bestAnswerId === undefined || bestAnswerId === null) {
+			return;
+		}
+
+		if (
+			this.props.bestAnswerId === undefined ||
+			this.props.bestAnswerId === null ||
+			!bestAnswerId.equals(this.props.bestAnswerId)
+		) {
+			this.addDomainEvent(
+				new QuestionBestAnswerChosenEvent(this, bestAnswerId),
+			);
+		}
+
+		this.props.bestAnswerId = bestAnswerId;
+
+		this.touch();
+	}
+
 	get title() {
 		return this.props.title;
 	}
 
+	set title(title: string) {
+		this.props.title = title;
+		this.props.slug = Slug.createFromText(title);
+
+		this.touch();
+	}
+
 	get content() {
 		return this.props.content;
+	}
+
+	set content(content: string) {
+		this.props.content = content;
+		this.touch();
 	}
 
 	get slug() {
@@ -40,6 +72,11 @@ export class Question extends AggregateRoot<QuestionProps> {
 
 	get attachments() {
 		return this.props.attachments;
+	}
+
+	set attachments(attachments: QuestionAttachmentList) {
+		this.props.attachments = attachments;
+		this.touch();
 	}
 
 	get createdAt() {
@@ -60,42 +97,6 @@ export class Question extends AggregateRoot<QuestionProps> {
 
 	private touch() {
 		this.props.updatedAt = new Date();
-	}
-
-	set title(title: string) {
-		this.props.title = title;
-		this.props.slug = Slug.createFromText(title);
-
-		this.touch();
-	}
-
-	set content(content: string) {
-		this.props.content = content;
-		this.touch();
-	}
-
-	set attachments(attachments: QuestionAttachmentList) {
-		this.props.attachments = attachments;
-		this.touch();
-	}
-
-	set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
-		if (bestAnswerId === undefined) {
-			return;
-		}
-
-		if (
-			this.props.bestAnswerId === undefined ||
-			!bestAnswerId.equals(this.props.bestAnswerId)
-		) {
-			this.addDomainEvent(
-				new QuestionBestAnswerChosenEvent(this, bestAnswerId),
-			);
-		}
-
-		this.props.bestAnswerId = bestAnswerId;
-
-		this.touch();
 	}
 
 	static create(
