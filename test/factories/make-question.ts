a9677d153e018761bc538140c3ddfa96417f4c58
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-03-20 18:25:22 +0000

    Add factories for creating questions and students in tests; enhance E2E tests for fetching question by slug

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,8 +3,11 @@ import { faker } from "@faker-js/faker";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import {
 	Question,
-	type QuestionProps,
+	QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
+import { PrismaQuestionMapper } from "@/infra/database/prisma/mappers/prisma-question-mapper";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
 
 export function makeQuestion(
 	override: Partial<QuestionProps> = {},
@@ -21,4 +24,21 @@ export function makeQuestion(
 	);
 
 	return question;
+}
+
+@Injectable()
+export class QuestionFactory {
+	constructor(private prisma: PrismaService) {}
+
+	async makePrismaQuestion(
+		data: Partial<QuestionProps> = {},
+	): Promise<Question> {
+		const question = makeQuestion(data);
+
+		await this.prisma.question.create({
+			data: PrismaQuestionMapper.toPrisma(question),
+		});
+
+		return question;
+	}
 }
