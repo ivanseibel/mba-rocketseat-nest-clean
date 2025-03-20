42a96a2e442273a46776e167718cd6a8b71f1f61
Ivan Luis Seibel <ivanseibel@gmail.com>
2025-03-20 18:38:00 +0000

    Refactor E2E tests to use factories for student and question creation; replace direct Prisma calls with factory methods

--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -4,8 +4,11 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 
 import {
 	QuestionComment,
-	type QuestionCommentProps,
+	QuestionCommentProps,
 } from "@/domain/forum/enterprise/entities/question-comment";
+import { PrismaQuestionCommentMapper } from "@/infra/database/prisma/mappers/prisma-question-comment-mapper";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
 
 export function makeQuestionComment(
 	override: Partial<QuestionCommentProps> = {},
@@ -22,4 +25,21 @@ export function makeQuestionComment(
 	);
 
 	return question;
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+	constructor(private prisma: PrismaService) {}
+
+	async makePrismaQuestionComment(
+		data: Partial<QuestionCommentProps> = {},
+	): Promise<QuestionComment> {
+		const questionComment = makeQuestionComment(data);
+
+		await this.prisma.comment.create({
+			data: PrismaQuestionCommentMapper.toPrisma(questionComment),
+		});
+
+		return questionComment;
+	}
 }
