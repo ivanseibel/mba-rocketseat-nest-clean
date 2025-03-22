28e49b90b073b7a51ed85004265a769e21703cf4
Ivan Seibel <me@ivanseibel.dev>
2025-03-22 14:50:45 +0000

    Implement DeleteAnswerComment use case and controller; add E2E tests for deleting answer comments

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,7 +1,8 @@
-import { type Either, left, right } from "@/core/either";
+import { Either, left, right } from "@/core/either";
 import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
 import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
-import type { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import { Injectable } from "@nestjs/common";
 
 interface DeleteAnswerCommentUseCaseRequest {
 	authorId: string;
@@ -13,6 +14,7 @@ type DeleteAnswerCommentUseCaseResponse = Either<
 	null
 >;
 
+@Injectable()
 export class DeleteAnswerCommentUseCase {
 	constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
