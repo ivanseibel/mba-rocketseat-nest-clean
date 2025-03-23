c194de8baee466594b81abc512839283374c5d61
Ivan Seibel <me@ivanseibel.dev>
2025-03-23 17:14:45 +0000

    Add createMany and deleteMany methods to QuestionAttachmentsRepository; implement tests for attachment persistence in question creation and editing

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,8 +1,8 @@
 import { DomainEvents } from "@/core/events/domain-events";
-import type { PaginationParams } from "@/core/repositories/pagination-params";
-import type { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
-import type { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
-import type { Question } from "@/domain/forum/enterprise/entities/question";
+import { PaginationParams } from "@/core/repositories/pagination-params";
+import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
+import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
+import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
 	public items: Question[] = [];
@@ -42,6 +42,10 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 	async create(question: Question) {
 		this.items.push(question);
 
+		await this.questionAttachmentsRepository.createMany(
+			question.attachments.getItems(),
+		);
+
 		DomainEvents.dispatchEventsForAggregate(question.id);
 	}
 
@@ -49,6 +53,14 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 		const itemIndex = this.items.findIndex((item) => item.id === question.id);
 
 		this.items[itemIndex] = question;
+
+		await this.questionAttachmentsRepository.createMany(
+			question.attachments.getNewItems(),
+		);
+
+		await this.questionAttachmentsRepository.deleteMany(
+			question.attachments.getRemovedItems(),
+		);
 
 		DomainEvents.dispatchEventsForAggregate(question.id);
 	}
